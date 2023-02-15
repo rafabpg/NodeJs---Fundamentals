b822311e2cfb733108e1ae5682ae4dfffb0df89f
Rafael Pillar <rafael.baptista@injunior.com.br>
2023-02-15 18:11:53 -0300

    feat: adding route file

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
-import { Database } from './database.js';
-import { randomUUID } from 'node:crypto';
+import { routes } from './routes.js';
+
+
 //common JS = require
 //ES/modules = import , porem o node nao suporta
 //para diferenciar um modulo terceiro para um modulo interno se utiliza a sintaxe com node:
@@ -13,7 +14,7 @@ import { randomUUID } from 'node:crypto';
 
 
 
-const database = new Database()
+
 const server  = http.createServer(async (request,response)=>{
     const { method,url} = request;
     // console.log(method,url)
@@ -21,23 +22,11 @@ const server  = http.createServer(async (request,response)=>{
     await json(request, response);
 
     console.log(fullBody.name);
-
-    if(method === 'GET' && url === '/users'){
-        const users = database.select('users');
-        return response.end(JSON.stringify(users));
-    } 
-    if(method === 'POST' && url === '/users') {
-        const { name,email} = request.body;
-        const user = {
-            id:randomUUID(),
-            name,
-            email
-        }
-        database.insert('users',user);
-        return response.writeHead(201).end()
-    }
-    return response.writeHead(404).end()
-
+    const route = routes.find(route=>{
+        return route.method == method && route.path == url;
+    })
+    if(route) return route.handler(request,response);
+    return response.writeHead(404).end();
 })
 
 server.listen(3333,()=>{console.log("server running")});
